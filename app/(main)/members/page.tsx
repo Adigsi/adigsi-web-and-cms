b66c84741adefa388db2524cea91e6c59cb18026
zulkifli-hs <itsmezulkifli@gmail.com>
2026-02-22 10:39:23 +0700

    feat: add Membership Benefits section to CMS Register page and update data fetching logic

--- app/(main)/members/page.tsx
+++ app/(main)/members/page.tsx
@@ -16,7 +16,6 @@ export default function MembersPage() {
       <MemberCategoriesSection />
       <DigitalMemberCategoriesSection />
       <PartnerLogosSection />
-      <MembershipBenefitsSection />
     </>
   )
 }
