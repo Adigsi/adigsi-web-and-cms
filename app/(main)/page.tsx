f6e22016fad39e4995815e079da1bcf025afc45f
zulkifli-hs <itsmezulkifli@gmail.com>
2026-02-24 15:34:35 +0700

    feat: comment out CarousellSection in Home component for future reference

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,12 +4,10 @@ import { LatestNewsSection } from '@/components/latest-news-section'
 import { IndustryReportSection } from '@/components/industry-report-section'
 import { AgendaSection } from '@/components/agenda-section'
 import { AboutAdigsiSection, PartnersSection } from '@/components/about-adigsi-section'
-import { CarousellSection } from '@/components/carousell-section'
 
 export default function Home() {
   return (
     <>
-      {/* <CarousellSection /> */}
       <HeroSection />
       <WelcomeSection />
       <LatestNewsSection />
